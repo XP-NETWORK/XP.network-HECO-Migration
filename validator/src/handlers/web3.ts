abfeda55afd72c18b905ae4302921af50b913adc
VKint <dima@xp.network>
2021-06-08 20:03:23 +0530

    validator: initial w3 handler

--- validator/src/handlers/web3.ts
+++ validator/src/handlers/web3.ts
@@ -1,20 +1,28 @@
 import { ChainListener, TransferEvent } from "validator";
 import Web3 from "web3";
 import { Contract } from "web3-eth-contract";
+import { Account } from "web3-core";
+import { AbiItem } from "web3-utils";
 
 
 export class Web3Helper implements ChainListener<TransferEvent> {
     private readonly w3: Web3;
     private readonly mintContract: Contract;
-    private readonly account: string;
+    private readonly account: Account;
 
-    private constructor(provider: Web3, mintContract: Contract, account: string) {
+    private constructor(provider: Web3, mintContract: Contract, account: Account) {
         this.w3 = provider;
         this.mintContract = mintContract;
         this.account = account;
     }
 
-    //public static async new(provider_uri: string, pkey: string, minter: string)
+    public static new(provider_uri: string, pkey: string, minter: string, minterAbi: AbiItem): Web3Helper {
+        const w3 = new Web3(provider_uri);
+        const acc = w3.eth.accounts.privateKeyToAccount(pkey);
+        const mint = new w3.eth.Contract(minterAbi, minter);
+
+        return new Web3Helper(w3, mint, acc);
+    }
 
     async emittedEventHandler(event: TransferEvent): Promise<void> {
         console.log("received event: ", event);
