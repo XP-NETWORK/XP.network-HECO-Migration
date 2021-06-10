2377a44e3342cd8be3d963b549abc57bc9e93889
VKint <dima@xp.network>
2021-06-10 20:19:44 +0530

    validator: initial listener

--- validator/src/handlers/web3.ts
+++ validator/src/handlers/web3.ts
@@ -1,11 +1,34 @@
-import { ChainListener, TransferEvent } from "validator";
+import { ChainEmitter, ChainListener, TransferEvent, UnfreezeEvent } from "validator";
 import Web3 from "web3";
 import { Contract } from "web3-eth-contract";
 import { Account } from "web3-core";
 import { AbiItem } from "web3-utils";
+import BN from "bn.js";
+import BigNumber from "bignumber.js";
 
+type SolUnfreezeEventArgs = {
+	readonly action_id: BN;
+	readonly to: string;
+	readonly value: BN;
+};
 
-export class Web3Helper implements ChainListener<TransferEvent> {
+type SolUnfreezeEvent = {
+	readonly event: string;
+	readonly signature: string | null;
+	readonly address: string;
+	readonly returnValues: SolUnfreezeEventArgs;
+	readonly logIndex: number;
+	readonly transactionIndex: number;
+	readonly transactionHash: string;
+	readonly blockHash: string;
+	readonly raw: {
+		data: string;
+		topics: Array<number>;
+	}
+};
+
+export class Web3Helper implements ChainEmitter<SolUnfreezeEvent, any, UnfreezeEvent>, ChainListener<TransferEvent> {
+	// @ts-expect-error
     private readonly w3: Web3;
     private readonly mintContract: Contract;
     private readonly account: Account;
@@ -17,18 +40,32 @@ export class Web3Helper implements ChainListener<TransferEvent> {
     }
 
     public static new(provider_uri: string, pkey: string, minter: string, minterAbi: AbiItem): Web3Helper {
-        const w3 = new Web3(provider_uri);
+        const w3 = new Web3(new Web3.providers.WebsocketProvider(provider_uri));
         const acc = w3.eth.accounts.privateKeyToAccount(pkey);
         const mint = new w3.eth.Contract(minterAbi, minter);
 
         return new Web3Helper(w3, mint, acc);
     }
 
+	async eventIter(cb: ((event: SolUnfreezeEvent) => Promise<void>)): Promise<any> {
+		this.mintContract.events.Unfreeze()
+		.on("data", async (ev: SolUnfreezeEvent | undefined) => {
+			console.log("ev", JSON.stringify(ev));
+			ev && ev.returnValues.action_id && await cb(ev)
+		});
+	}
+
+	async eventHandler(ev: SolUnfreezeEvent): Promise<UnfreezeEvent | undefined> {
+		const args = ev.returnValues;
+		const action_id = new BigNumber(args.action_id.toString());
+		const value = new BigNumber(args.value.toString());
+		return new UnfreezeEvent(action_id, args.to, value);
+	}
+
     async emittedEventHandler(event: TransferEvent): Promise<void> {
         await this.mintContract.methods.validate_transfer(event.action_id, event.to, event.value)
             .send({from: this.account.address})
             .on('transactionHash', (hash: string) => console.log("Validate Transfer hash: ", hash))
-            .on('confirmation', (receipt: any) => console.log(`Validate transfer receipt: ${JSON.stringify(receipt)}`))
             .on('error', (err: Error, _: any) => { throw err })
     }
 }
--- validator/src/index.ts
+++ validator/src/index.ts
@@ -1,1 +1,28 @@
+import {PolkadotHelper, emitEvents} from "validator";
+import {Web3Helper} from "./handlers/web3";
+import * as freezer_abi from './freezer_abi.json';
+import { abi as minter_abi  }  from './minter.json';
+import config from './config';
 
+const main = async () => {
+	const polka = await PolkadotHelper.new(
+		config.xnode,
+		freezer_abi,
+		config.xp_freezer
+	);
+
+	const w3 = Web3Helper.new(
+		config.w3_node,
+		config.w3_pk,
+		config.w3_minter,
+		//@ts-expect-error
+		minter_abi
+	)
+
+	console.log('READY TO LISTEN EVENTS!');
+
+	emitEvents(polka, w3);
+	emitEvents(w3, polka);
+};
+
+main();
