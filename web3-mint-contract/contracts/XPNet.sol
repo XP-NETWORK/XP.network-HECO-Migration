5b042b590e33e5c0e3ceca415c786210dd2e5e33
VKint <dima@xp.network>
2021-06-21 14:00:59 +0530

    XPNet: set initial mint value to 0

--- web3-mint-contract/contracts/XPNet.sol
+++ web3-mint-contract/contracts/XPNet.sol
@@ -4,9 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract XPNet is ERC20, Ownable {
-    constructor() ERC20("XPNet", "XPN") {
-        _mint(msg.sender, 1 * 10 ** decimals());
-    }
+    constructor() ERC20("XPNet", "XPN") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
