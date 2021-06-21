ba7b53c4c55ecf0ea39c368e168f52468360a5e2
VKint <dima@xp.network>
2021-06-21 18:47:01 +0530

    validator: config: use json

--- validator/src/config.ts
+++ validator/src/config.ts
@@ -1,16 +1,3 @@
-const config = {
-    // Substrate Node Uri
-    xnode: 'ws://localhost:9944',
-    // Web3 node Uri
-    w3_node: 'ws://localhost:8545',
-    // Private Key str
-	w3_pk:
-        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-    // web3 minter contract address
-    w3_minter:
-        '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-    // Substrate Freezer Contract address
-    xp_freezer: '5F9tD87UYEKzzxMBAzMkkuX9xBKD7s586N2SJpfCUHSTs55f',
-};
+import * as config from "./config.json"
 
 export default config;
