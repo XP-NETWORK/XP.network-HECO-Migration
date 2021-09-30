a1456472f6c5c83bea58e1a1162aa4d335718e9d
imsk17 <imsumitkumar17@gmail.com>
2021-09-30 15:43:44 +0530

    feat(contracts): Add a UserMinter contract

---
+++ web3-mint-contract/contracts/UserNftMinter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract UserNftMinter is ERC721URIStorage, Ownable {
+    constructor() ERC721("UserNftMinter", "UMT") {}
+
+    function mint(uint256 id, string calldata uri) public {
+        _safeMint(msg.sender, id);
+        _setTokenURI(id, uri);
+    }
+
+    function burn(uint256 id) public {
+        address owner = ownerOf(id);
+        require(owner == msg.sender, "You are not the owner of this token");
+        _burn(id);
+    }
+}
