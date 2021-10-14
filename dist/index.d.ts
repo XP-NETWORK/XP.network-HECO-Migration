80749689f97a08e5dca8efc2600b25c91cb906c8
rupansh <rupanshsekar@hotmail.com>
2021-10-14 12:54:36 +0000

    Build: (5a60e2e) add .env.example
    fixes workflow

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export type { Minter } from "./Minter";
+export type { UserNftMinter } from "./UserNftMinter";
+export type { XPNet } from "./XPNet";
+export type { XPNft } from "./XPNft";
+export { Minter__factory } from "./factories/Minter__factory";
+export { UserNftMinter__factory } from "./factories/UserNftMinter__factory";
+export { XPNet__factory } from "./factories/XPNet__factory";
+export { XPNft__factory } from "./factories/XPNft__factory";
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.XPNft__factory = exports.XPNet__factory = exports.UserNftMinter__factory = exports.Minter__factory = void 0;
+var Minter__factory_1 = require("./factories/Minter__factory");
+Object.defineProperty(exports, "Minter__factory", { enumerable: true, get: function () { return Minter__factory_1.Minter__factory; } });
+var UserNftMinter__factory_1 = require("./factories/UserNftMinter__factory");
+Object.defineProperty(exports, "UserNftMinter__factory", { enumerable: true, get: function () { return UserNftMinter__factory_1.UserNftMinter__factory; } });
+var XPNet__factory_1 = require("./factories/XPNet__factory");
+Object.defineProperty(exports, "XPNet__factory", { enumerable: true, get: function () { return XPNet__factory_1.XPNet__factory; } });
+var XPNft__factory_1 = require("./factories/XPNft__factory");
+Object.defineProperty(exports, "XPNft__factory", { enumerable: true, get: function () { return XPNft__factory_1.XPNft__factory; } });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBUUEsK0RBQThEO0FBQXJELGtIQUFBLGVBQWUsT0FBQTtBQUN4Qiw2RUFBNEU7QUFBbkUsZ0lBQUEsc0JBQXNCLE9BQUE7QUFDL0IsNkRBQTREO0FBQW5ELGdIQUFBLGNBQWMsT0FBQTtBQUN2Qiw2REFBNEQ7QUFBbkQsZ0hBQUEsY0FBYyxPQUFBIn0=
