532676d899525250cc2d36f285b36b9a690e8812
imsk17 <imsumitkumar17@gmail.com>
2022-11-01 10:59:29 +0000

    Build: (393e818) contracts: minter-erc20: add a minter with erc20 support As a payment method.

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.XPNft1155__factory = exports.XPNft__factory = exports.UserNftMinter__factory = exports.SchnorrSECP256K1__factory = exports.Minter__factory = exports.Erc1155Minter__factory = exports.BridgeNFTBatch__factory = exports.BridgeNFT__factory = void 0;
+exports.XPNft1155__factory = exports.XPNft__factory = exports.UserNftMinter__factory = exports.SchnorrSECP256K1__factory = exports.MinterERC20__factory = exports.Minter__factory = exports.Erc1155Minter__factory = exports.BridgeNFTBatch__factory = exports.BridgeNFT__factory = void 0;
 var BridgeNFT__factory_1 = require("./factories/BridgeNFT__factory");
 Object.defineProperty(exports, "BridgeNFT__factory", { enumerable: true, get: function () { return BridgeNFT__factory_1.BridgeNFT__factory; } });
 var BridgeNFTBatch__factory_1 = require("./factories/BridgeNFTBatch__factory");
@@ -9,6 +9,8 @@ var Erc1155Minter__factory_1 = require("./factories/Erc1155Minter__factory");
 Object.defineProperty(exports, "Erc1155Minter__factory", { enumerable: true, get: function () { return Erc1155Minter__factory_1.Erc1155Minter__factory; } });
 var Minter__factory_1 = require("./factories/Minter__factory");
 Object.defineProperty(exports, "Minter__factory", { enumerable: true, get: function () { return Minter__factory_1.Minter__factory; } });
+var MinterERC20__factory_1 = require("./factories/MinterERC20__factory");
+Object.defineProperty(exports, "MinterERC20__factory", { enumerable: true, get: function () { return MinterERC20__factory_1.MinterERC20__factory; } });
 var SchnorrSECP256K1__factory_1 = require("./factories/SchnorrSECP256K1__factory");
 Object.defineProperty(exports, "SchnorrSECP256K1__factory", { enumerable: true, get: function () { return SchnorrSECP256K1__factory_1.SchnorrSECP256K1__factory; } });
 var UserNftMinter__factory_1 = require("./factories/UserNftMinter__factory");
@@ -17,4 +19,4 @@ var XPNft__factory_1 = require("./factories/XPNft__factory");
 Object.defineProperty(exports, "XPNft__factory", { enumerable: true, get: function () { return XPNft__factory_1.XPNft__factory; } });
 var XPNft1155__factory_1 = require("./factories/XPNft1155__factory");
 Object.defineProperty(exports, "XPNft1155__factory", { enumerable: true, get: function () { return XPNft1155__factory_1.XPNft1155__factory; } });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBWUEscUVBQW9FO0FBQTNELHdIQUFBLGtCQUFrQixPQUFBO0FBQzNCLCtFQUE4RTtBQUFyRSxrSUFBQSx1QkFBdUIsT0FBQTtBQUNoQyw2RUFBNEU7QUFBbkUsZ0lBQUEsc0JBQXNCLE9BQUE7QUFDL0IsK0RBQThEO0FBQXJELGtIQUFBLGVBQWUsT0FBQTtBQUN4QixtRkFBa0Y7QUFBekUsc0lBQUEseUJBQXlCLE9BQUE7QUFDbEMsNkVBQTRFO0FBQW5FLGdJQUFBLHNCQUFzQixPQUFBO0FBQy9CLDZEQUE0RDtBQUFuRCxnSEFBQSxjQUFjLE9BQUE7QUFDdkIscUVBQW9FO0FBQTNELHdIQUFBLGtCQUFrQixPQUFBIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBYUEscUVBQW9FO0FBQTNELHdIQUFBLGtCQUFrQixPQUFBO0FBQzNCLCtFQUE4RTtBQUFyRSxrSUFBQSx1QkFBdUIsT0FBQTtBQUNoQyw2RUFBNEU7QUFBbkUsZ0lBQUEsc0JBQXNCLE9BQUE7QUFDL0IsK0RBQThEO0FBQXJELGtIQUFBLGVBQWUsT0FBQTtBQUN4Qix5RUFBd0U7QUFBL0QsNEhBQUEsb0JBQW9CLE9BQUE7QUFDN0IsbUZBQWtGO0FBQXpFLHNJQUFBLHlCQUF5QixPQUFBO0FBQ2xDLDZFQUE0RTtBQUFuRSxnSUFBQSxzQkFBc0IsT0FBQTtBQUMvQiw2REFBNEQ7QUFBbkQsZ0hBQUEsY0FBYyxPQUFBO0FBQ3ZCLHFFQUFvRTtBQUEzRCx3SEFBQSxrQkFBa0IsT0FBQSJ9
